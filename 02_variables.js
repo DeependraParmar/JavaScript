a27247044a266a0d19f5406a54ae538ab7703aa6
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-03-23 05:22:03 +0530

    Add files via upload

---
+++ 02_variables.js
@@ -0,0 +1,8 @@
+// Javascript is a dynamically typed language as it allows the change in its type at runtime 
+// Variables are the containers that are basically used to store some type of value. Variables are nothing but a memory allocated for the storage of the value or data 
+
+// we are having two keywords that is var and let for defining the variables in javascript 
+var a = "Deependra";
+a = 20;
+
+console.log(a);
