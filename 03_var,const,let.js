a027b8aa94ce591748cf8433573ba6af4fb5f235
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-04-11 19:47:24 +0530

    Add files via upload

---
+++ 03_var,const,let.js
@@ -0,0 +1,7 @@
+// var is not used for defining variables in modern practices of javascript because there are huge chances of introduction of bug in your code 
+
+// var is globally scoped and let,const are block or local scoped. 
+// var can be redeclared but let and const cannot be redefined 
+// value of const cannot be updated.
+
+
