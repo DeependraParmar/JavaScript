848dd781a8aca59b2d353d7941fd9c685f075f1a
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-04-14 08:48:58 +0530

    Add files via upload

---
+++ 07_conditionals.js
@@ -0,0 +1,8 @@
+// Conditional statements are special statements which execute a particular block of code based upon the condition 
+//1. if statement
+//2. if else statement
+//3. if elseif else statements
+
+// Ternary Operator: Shortcut for if else 
+const a = 10;
+console.log(a>5?"Max":"Min");
