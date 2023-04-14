31283920fef024ad8b8b151b0d3d4a4c868b7cce
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-04-14 08:48:34 +0530

    Add files via upload

---
+++ 06_expressions_and_operators.js
@@ -0,0 +1,16 @@
+let x = 20;
+let y = 10;
+
+// Arithmetic Operators : +,-,*,/,%,**,++,--
+console.log("Addition: ",(x+y));
+console.log("Subtraction: ",(x-y));
+console.log("Multiplication: ",(x*y));
+console.log("Division: ",(x/y));
+console.log("Remainder: ",(x%y));
+console.log("Exponentiation: ",(x**y));
+
+
+// Assignment operators: ==,+=,-=,*=,/=,%=
+//Comparision or Relational Operators: ==,!=,===(checks according to value as well as datatype),!==,>,<,>=,<=,?
+//Logical Operators: &&,||,!
+//Bitwise operators: &,|,^,>>,<<,etc
