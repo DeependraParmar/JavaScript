07a224e7c2d253e38e1481f0a86456f6d5bea831
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-04-28 06:38:51 +0530

    Add files via upload

---
+++ 13_strings.js
@@ -0,0 +1,17 @@
+let naam = "Deependra";
+console.log("The length of the string is: " + naam.length);
+let friend = 'Aditya';
+console.log("The length of the friend is: " + friend.length)
+
+// We have the concept of Template Literal which is used to store a particular string into a variable and the variable can be used access the whole string
+let info = `We have the concept of Template Literal which is used to store a particular string into a variable and the variable can be used access the whole string`;
+console.log(`Information is: ${info}`);
+
+// We can use directly the variables in the string using Template Literals known as String Interpolation 
+
+// Escape Sequence Characters 
+let fruit = "Man\'go";
+console.log("Name of the fruit is: " + fruit);
+console.log("Length of the fruit is: " + fruit.length);
+
+// \n for new line, \t for tab, \r for carriage return
