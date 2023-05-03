4094863bfdb7a81876d3e4dee560d02ee6d907c5
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-05-03 10:05:35 +0530

    Add files via upload

---
+++ 24_scripttag.js
@@ -0,0 +1,5 @@
+console.log("Hello, we are going to know about script tag in javascript");
+
+// Advantages of using seperate script or js files :
+    // 1. seperation of concerns
+    // 2. browser caching 
