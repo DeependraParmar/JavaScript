25ca7d1313934a39beb6c81828b0340be7d9698d
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-05-03 10:05:01 +0530

    Add files via upload

---
+++ 23_jsInBrowser.js
@@ -0,0 +1,3 @@
+// Js was initially created to make the web pages live. Browser has embedded engine known as Javascript engine or runtime 
+// Developer Tools: elements, console, network are most important
+
