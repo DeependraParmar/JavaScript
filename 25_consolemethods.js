7fc991857e61a2bd7b3f35b7dc947b93ad4f775f
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-05-04 19:58:43 +0530

    Add files via upload

---
+++ 25_consolemethods.js
@@ -0,0 +1,14 @@
+// console methods in js 
+console.log("Hello guys, I am Deependra Parmar");
+console.error("This is an error");
+console.assert(50>5);
+console.clear();//used to clear the console and state that console was cleared
+
+const obj ={
+    1:"hello",
+    2:"guys",
+    3:"bye"
+}
+console.table(obj); 
+console.warn("Hey dont drink coffee");
+console.info("This is an information");// similar to console.log
