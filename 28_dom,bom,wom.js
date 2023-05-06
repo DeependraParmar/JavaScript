3ffdded659a28e47d2d0bfbc7ffb77de066f2ab0
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-05-06 15:33:14 +0530

    Add files via upload

---
+++ 28_dom,bom,wom.js
@@ -0,0 +1,10 @@
+// Window is a global object of browser or say javascript in which dom, bom , and javascript ke core features resides 
+console.log(window);
+
+// DOM: DOM stands for document object model which is an object of your whole html page and names as document
+console.log(document);//shows document on console
+console.log(document.body);// shows body on console
+document.body.style.background = "yellow";
+
+
+// bom: browser object model represents additional object provided by the browser or say host environment
