26d6edf21511b905d4be1bb147e1d06bd1c675df
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-05-10 20:16:16 +0530

    Add files via upload

---
+++ 31_walkingThedom.js
@@ -0,0 +1,25 @@
+// dom refers to the object which is the collection of the html page 
+// we can use dom to manipulate the dom using javascript 
+
+// text node: tag containing text  which is always a leaf node of dom tree
+// element node: tags which are used to derive elements like buttons, etc
+// comment node: comments are comment nodes 
+
+
+// Auto Correction in Html: Whenever browser encounter errornous HTML, browser tends to auto correct it. Eg. you started and <div> and closed it with </span> then, it will auto correct the error and correct it that is browser is going to close the <div> with </div> itself. <tbody> must be just after <table>.
+
+
+// Walking the dom 
+// dom>html>head>body
+
+//document.documentElement --> returns the html(object)
+//document.body --> returns the body(object) of the whole page
+//document.head --> returns the head(object) of the whole page 
+//document.title --> returns the string that is the title of the html page
+
+
+// Note: document.body can sometimes be null if javascript is written before the body tag 
+
+
+
+// Children of an element:: -------->
